6b2498f424340c65c1a0e77a54e58b1a215fe0f0
yesidcs <yesidc@gmail.com>
2026-08-26 17:06:10 -0500

    Diseño inicial landing Page.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component')
+        .then(m => m.HomeComponent)
+  }
+];
